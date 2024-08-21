e4e6bd7ae0f18b5219c48470d51123629efd1cb9
OlegKumachev <117755901+OlegKumachev@users.noreply.github.com>
2024-08-21 12:16:51 +0300

    Update vite.config.js

--- Frondend/MyCloud/vite.config.js
+++ Frondend/MyCloud/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/DiplomMyCloud/Frondend/MyCloud/',
   build: {
     outDir: 'dist'
   }
